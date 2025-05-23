77e01af614822b13ad94a45dae7e6876d1698145
ThePhysic <57155067+ThePhysic@users.noreply.github.com>
2025-05-23 02:49:16 -0700

    Implemented inversion feature

--- index.js
+++ index.js
@@ -20,9 +20,19 @@ client.once(Events.ClientReady, readyClient => {
     // Create ping slash command
     const ping = new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!');
+        .setDescription('Replies with Pong!')
+    
+    // Create inversion slash command
+    const invert = new SlashCommandBuilder()
+        .setName('inversion')
+        .setDescription('Inverts the message you send')
+        .addStringOption(option =>
+            option.setName('message')
+                .setDescription('The message to be inverted')
+                .setRequired(true));
     
     client.application.commands.create(ping);
+    client.application.commands.create(invert);
 });
 
 // Listen for slash commands
@@ -31,6 +41,26 @@ client.on(Events.InteractionCreate, interaction => {
     // Ping slash command
     if (interaction.commandName === 'ping') {
         interaction.reply('Pong!');
+    }
+
+    // inversion slash command
+    if (interaction.commandName === 'inversion') {
+        const message = interaction.options.get('message').value;
+        
+        let stack = [];
+        let invertedMessage = "";
+        
+        // Add characters in message to stack
+        for (const char of message) {
+            stack.push(char);
+        }
+
+        // Reverse order of characters from message
+        for (let char = 0; stack.length; char++) {
+            invertedMessage += stack.pop();
+        }
+
+        interaction.reply(invertedMessage);
     }
 })
 
